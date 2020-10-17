09ec666b8641c0518d5306101da0bee61a2fa83c
Alok nayan <alok.nayan2016@gmail.com>
2020-10-17 11:26:33 +0530

    created Middleware

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
+
+
+// Database Connection
 mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -10,6 +16,11 @@ mongoose.connect(process.env.DATABASE, {
 }).then(() => {
     console.log("DB CONNECTED")
 })
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 const port = process.env.PORT || 8000;
 
