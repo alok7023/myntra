b7549d050cca590394f0ddeee7c44ecf8077aadf
Alok nayan <alok.nayan2016@gmail.com>
2020-10-17 21:39:26 +0530

    created signin route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const { check, validationResult } = require('express-validator');
+var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 exports.signup = (req, res) => {
     const errors = validationResult(req);
@@ -24,11 +26,45 @@ exports.signup = (req, res) => {
       });
 };
 
+exports.signin = (req, res) => {
+  const errors = validationResult(req);
+  const { email, password } = req.body;
 
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+    });
+  }
 
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'USER email does not exists',
+      });
+    }
+
+    if (!user.autheticate(password)) {
+      return res.status(401).json({
+        error: 'Email and password do not match',
+      });
+    }
+
+  //create token
+  const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+  
+  //put token in cookie
+  res.cookie('token', token, { expire: new Date() + 9999 });
+
+  //send response to front end
+  const { _id, name, email, role } = user;
+  return res.json({ token, user: { _id, name, email, role } });
+
+  });
+}
 
 exports.signout = (req, res) => {
     res.json({
         message: "User signout"
     });
 };
+
